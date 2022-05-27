cd0a1fc77cfb063e6561e3fd884ef614bbfd5e92
Jihoon Gweon <kjh8673a@naver.com>
2022-05-27 17:55:35 +0900

    jsp 빌드 그래들

---
+++ src/main/java/com/Modoomoyeo/momo/main/MainController.java
@@ -0,0 +1,18 @@
+package com.Modoomoyeo.momo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public ModelAndView main() {
+        ModelAndView mav = new ModelAndView();
+
+        mav.setViewName("main/main");
+        return mav;
+    }
+}
