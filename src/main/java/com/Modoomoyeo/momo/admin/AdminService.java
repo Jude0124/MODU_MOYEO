f8e9542b8b4122b9d87877bb2df68a0837f50a0f
didwltjr93 <didwltjr93@gmail.com>
2022-06-03 13:53:13 +0900

    보드수정

--- src/main/java/com/Modoomoyeo/momo/admin/AdminService.java
+++ src/main/java/com/Modoomoyeo/momo/admin/AdminService.java
@@ -1,4 +1,11 @@
 package com.Modoomoyeo.momo.admin;
 
+import com.Modoomoyeo.momo.user.UserVO;
+
+import java.util.List;
+
 public interface AdminService {
+    public int totalRecordUser();
+    public List<UserVO> adminUsers();
+    public int adminUsersDel(UserVO uvo);
 }
--- src/main/java/com/Modoomoyeo/momo/user/UserService.java
+++ src/main/java/com/Modoomoyeo/momo/user/UserService.java
@@ -1,4 +1,5 @@
 package com.Modoomoyeo.momo.user;
 
 public interface UserService {
+
 }
