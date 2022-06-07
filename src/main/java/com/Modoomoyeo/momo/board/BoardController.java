f6635bf2ac49f5868b8b9599a40ae2e86a9028b9
aseungy <ysa2414@naver.com>
2022-06-07 09:42:56 +0900

    게시글 등록 오류 해결

--- src/main/java/com/Modoomoyeo/momo/board/BoardController.java
+++ src/main/java/com/Modoomoyeo/momo/board/BoardController.java
@@ -36,8 +36,6 @@ public class BoardController {
         ModelAndView mav = new ModelAndView();
 
         bpvo.setTotalRecord(boser.boardTotalRecord(bpvo));
-        model.addAttribute("list",boser.boardList(bpvo));
-        model.addAttribute("bpvo", bpvo);
 
 
         mav.addObject("list", boser.boardList(bpvo));
@@ -73,13 +71,14 @@ public class BoardController {
 
 
     @PostMapping ("/boardWriteOK")
-    public ModelAndView boardwriteok(HttpServletResponse response, BoardVO bvo) throws IOException {
+    public void boardwriteok(HttpServletResponse response, BoardVO bvo) throws IOException {
         ModelAndView mav = new ModelAndView();
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
         boser.boardInsert(bvo);
         out.print("<script>location.href='/boardList'</script>");
         out.flush();
+    }
 
     /*게시글 삭제*/
     @GetMapping("/board/contentDel")
